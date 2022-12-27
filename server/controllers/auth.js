cc14be8efe86e10cd59162528689d68ffa9ad368
Harry Smith <harryawsmith@gmail.com>
2022-12-27 17:08:19 +1100

    initial commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -31,9 +31,9 @@ export const register = async (req, res) => {
         occupation,
         viewedProfile: Math.floor(Math.random() * 10000),
         impressions: Math.floor(Math.random() * 10000)
-     }):
+     });
      const savedUser = await newUser.save();
-     res.status(201).json(savedUser);
+     res.status(201).json(savedUser); 
     } catch (err) {
 
     }
